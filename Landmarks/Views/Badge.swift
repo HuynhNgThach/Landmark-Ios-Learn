85757a7dd2f90eb4eb1170b0978593fee0c5741a
thachhn <huynhngthach@gmail.com>
2023-12-27 13:28:01 +0700

    Add path drawing, not really understand how it works

---
+++ Landmarks/Views/Badge.swift
@@ -0,0 +1,18 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Thach Huynh on 27/12/2023.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Badge()
+}
---
+++ Landmarks/Views/HexagonParameters.swift
@@ -0,0 +1,8 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by Thach Huynh on 27/12/2023.
+//
+
+import Foundation
