e38266aeb8075901ed49d2aa580768685ee76cef
thachhn <huynhngthach@gmail.com>
2023-12-29 10:02:01 +0700

    Add categories section

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Thach Huynh on 27/12/2023.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryHome()
+}
---
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Thach Huynh on 28/12/2023.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryRow()
+}
